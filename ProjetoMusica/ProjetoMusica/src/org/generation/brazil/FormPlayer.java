2f4e79e6b03306da5f22413837851bb773134179
Beatriz Moreli <79378960+Beamoreli@users.noreply.github.com>
2022-03-11 15:28:01 -0300

    Projeto Gen Music

---
+++ ProjetoMusica/ProjetoMusica/src/org/generation/brazil/FormPlayer.java
@@ -0,0 +1,21 @@
+package org.generation.brazil;
+
+public class FormPlayer implements Player {
+
+	private int iniciar = 0;
+	
+	@Override
+	public void iniciar() { // Polimorfismo
+
+		System.out.println("\n⏮ ▶️ ⏸️ ⏹️ ⏭️");
+	
+	}
+
+	public int getIniciar() { //Encapsulamento 
+		return iniciar;
+	}
+
+	public void setIniciar(int iniciar) {
+		this.iniciar = iniciar;
+	}
+}
---
+++ ProjetoMusica/ProjetoMusica/src/org/generation/brazil/Player.java
@@ -0,0 +1,7 @@
+package org.generation.brazil;
+
+public interface Player {
+	
+	public abstract void iniciar();
+	
+}
